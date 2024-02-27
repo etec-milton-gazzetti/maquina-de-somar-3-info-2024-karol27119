8b247607a2375e4af9362a4304b22cadb84bd378
karol27119 <116905552+karol27119@users.noreply.github.com>
2024-02-27 12:22:20 +0000

    {feat-textosdapaginainicial}

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Etec Professor Milton Gazzeti</Text>
+      <Text style={styles.fonte}>3 ano</Text>
+      <Text>Nós somos<Text style={styles.underline}> O 3 INFO</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  fonte: {
+    fontStyle:'italic',
+  },
+  underline: {
+    textDecorationLine: 'underline',
   },
 });
